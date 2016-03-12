dac636a6a12d4904677037ef32c1dd01eb0cb3b5
ammc <alexander.hdwctw@gmail.com>
2016-03-12 11:25:35 -0800

    barely stable update

--- src/app/main/main.controller.js
+++ src/app/main/main.controller.js
@@ -3,69 +3,22 @@
 
   angular
     .module('kelley')
-    .controller('ModalController', ModalController)
-    .controller('NavController', NavController)
-
-    .controller('MainController', MainController)
-    .controller('MlsController', MlsController)
-    .controller('SellerController', SellerController)
-    .controller('BuyerController', BuyerController)
-    .controller('BackgroundController', BackgroundController)
-    .controller('TestimonialsController', TestimonialsController)
+        .controller('MainController', MainController)
 
   /** @ngInject */
-  var last = "";
-  function ModalController($timeout){
+  function MainController($timeout, webDevTec, toastr) {
       $(document).ready(function(){
         $('.cModal').hide();
       })
-  }
-
-  function NavController($scope){
-    $scope.test = function(event){
-      $(this).parent().addClass('active');
-    }
-  }
-
-  function MlsController($scope){
-    $(last).removeClass('active');
-    $("#mlsli").addClass('active');
-    last = '#mlsli';
-  }
-  function SellerController(){
-    $(last).removeClass('active');
-    $("#sellerli").addClass('active');
-    last = '#sellerli';
-  }
-  function BuyerController(){
-    $(last).removeClass('active');
-    $("#buyerli").addClass('active');
-    last = '#buyerli';
-  }
-  function BackgroundController(){
-    $(last).removeClass('active');
-    $("#backgroundli").addClass('active');
-    last = '#backgroundli';
-  }
-  function TestimonialsController(){
-    $(last).removeClass('active');
-    $("#testili").addClass('active');
-    last = '#testili';
-  }
-  function MainController($timeout, webDevTec, toastr) {
-    $(last).removeClass('active');
-    $("#featuredli").addClass('active');
-    last = '#featuredli';
-
-    $('.cccontact').click(function(){
-      console.log('wat');
-      $('.cModal').fadeIn('slow');
-      console.log('waddt');
-      return false;
-    });
-    $('#sendemail').on('click', function(){
-      $('.cModal').fadeOut('slow');
-    });
+      $('.cccontact').click(function(){
+        console.log('wat');
+        $('.cModal').fadeIn('slow');
+        console.log('waddt');
+        return false;
+      });
+      $('#sendemail').on('click', function(){
+        $('.cModal').fadeOut('slow');
+      });
 
 
     var vm = this;
